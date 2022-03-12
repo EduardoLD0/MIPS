4b0344d1e792dda61a4ffcc8b24f0e8ce66ff674
EduardoLD0 <66051048+EduardoLD0@users.noreply.github.com>
2022-03-11 23:14:13 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,16 +1,15 @@
 # Programa para traducir instrucciones de MIPS y calcular el tiempo de ejecución
-#from traductor import traducir as tr
-baseAdd = 0
+import traductor
+from traductor import traducir
 
 def main():
     file = open("mips.txt", "r") # Abrir archivo con instrucciones
     for i in file:
-        tr(i)
-        instPoint+=4 #Incrementar puntero de instrucciones
+        traducir(i) 
         pass
 
     
 
-    print("{}".format(bin(4).replace("0b", "").zfill(5)))
+    #print("{}".format(bin(4).replace("0b", "").zfill(5)))
 
 main()
--- traductor.py
+++ traductor.py
@@ -1,63 +1,107 @@
 from datos import functR, opI, opJ, registros
-instAdd = {}
+#Manejo de direcciones de instrucciones
+instAdd = {} #diccionario de etiquetas con su direccion
+instPoint = 0 #numero de la instruccion
+baseAdd = 0
+#Contador de ciclos
+numCiclos = 0
+
+# Imprimir binario
+def pBin(n,p):
+    if int(n) < 0:
+        binary = str("{}".format(bin(int((1<<int(p)) - abs(int(n)))).replace("0b", "")))
+    else:
+        binary = str("{}".format(bin(int(n)).replace("0b", "").zfill(p)))
+    return binary
 
 def traducir(ins):
-    ins = ins.split() # ["add", "$s1", "$s2", "$s3"]
-    reg = ins[1].split(",")
+    global instPoint, numCiclos
+    ins = ins.replace("(", " ").replace(")", " ")
+    ins = ins.replace(",", " ").split() # ["add", "$s1", "$s2", "$s3"]
+    print(ins)
     # Traducir tipo R
     if ins[0] in functR:
-        # Traduccion estandar tipo R
-        op = 0b000000
-        rd = registros[reg[0]]
-        rs = registros[reg[1]]
-        rt = registros[reg[2]]
-        shamt = 0b000000
+        op = "000000"
         funct = functR[ins[0]]
         # Caso sll y srl
         if ins[0] == "sll" or ins[0] == "srl":
-            rs = 0b00000
-            shamt = reg[3]
+            rs = "00000"
+            rt = registros[ins[2]]
+            rd = registros[ins[1]]
+            shamt = ins[4]
         # Caso jr
         elif ins[0] == "jr":
-            rs = registros[reg[0]]
-            rt = 0b00000
-            rd = 0b00000
+            rs = registros[ins[1]]
+            rt = rd = shamt = "00000"
         # Caso mfc0
         elif ins[0] == "mfc0":
-            op = 0b010000
-            funct = 0b000000
-            rd = bin(reg[0])
+            op = "010000"
+            funct = "000000"
+            rd = pBin(ins[1],5)
+            rs = rt = "00000"
         # Caso mflo y mfhi
         elif ins[0] == "mflo" or ins[0] == "mfhi":
-            rd = bin(reg[0])
-        return op + rs + rt + rd + shamt + funct
+            rd = pBin(ins[1],5)
+            rs = rt = "00000"
+        else:
+        # Traduccion estandar tipo R
+            rd = registros[ins[1]]
+            rs = registros[ins[2]]
+            rt = registros[ins[3]]
+            shamt = "00000"
+
+        #Incrementar puntero de instrucciones
+        instPoint+=4 
+        #Contar ciclos
+        if ins[0] == "lw": numCiclos += 5
+        else: numCiclos += 4
+
+        return op + " "+ rs +" "+ rt +" "+ rd +" "+ shamt +" "+ funct
     
     elif ins[0] in opI:
         # Traducción general tipo I
         op = opI[ins[0]]
-        rt = registros[reg[0]]
-        rs = registros[reg[1]]
-        immediate = bin(reg[2])
+        rt = registros[ins[1]]
+        rs = registros[ins[3]]
+        immediate = pBin(ins[2],16)
         # Caso lui
         if ins[0] == "lui":
-            rs = 0b00000
+            rs ="00000"
         # Caso lw, sw, lbu y sb
         elif ins[0] == "lw" or ins[0] == "sw" or ins[0] == "lbu" or ins[0] == "sb":
-            immediate = bin(reg[1])
-            rs = registros[reg[2]]
+            immediate = pBin(ins[2],16)
+            rs = registros[ins[3]]
+
+        #Incrementar puntero de instrucciones
+        instPoint+=4 
+        #Contar ciclos
+        if ins[0] == "beq" or ins[0] == "bne": numCiclos += 3
+        else: numCiclos += 4
+
+        return op +" "+ rs +" "+ rt +" "+ immediate
     
     elif ins[0] in opJ:
         # Traduccion general tipo J
         op = opJ[ins[0]]
-        address = bin(ins[1])
+        address = pBin(ins[1],26)
+
+        #Incrementar puntero de instrucciones
+        instPoint+=4 
+        #Contar ciclos
+        numCiclos += 3
+
+        return op +" "+ address
 
     elif ins[0][-1] == ":": #Si es un label
         label = ins[0].replace(":", "")
-        instAdd[label] = 
-        
-    else:
-        print("Operacion inválida")
+        instAdd[label] = instPoint #Agrega esta etiqueta con su direccion al arreglo
+        print(instAdd)
+        return None
+    else: #Si es comentario, linea en blanco, o instruccion no valida
+        return None
 
-    
 
+
+print(traducir("lw $s1, 5($s2)"))
+print(traducir("Main:"))
 
